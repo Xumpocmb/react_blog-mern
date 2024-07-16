311330b841bae735da567a8ae1a82ea71c2e89fa
xumpocmb <x.xumpocmb@gmail.com>
2024-07-16 09:32:52 +0300

    test post request

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
 });
 
-app.listen(4444, (err) => {
-    if (err) {
-        return console.log("something bad happened:", err);
-    }
-
-    console.log("Server started on port 4444");
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+	res.json({
+		token: '1234',
+		user: {
+			id: 1,
+			name: 'John Doe',
+			username: 'johndoe',
+		},
+		message: 'Login successful',
+		success: true,
+	});
 });
+
+app.listen(4444, err => {
+	if (err) {
+		return console.log('something bad happened:', err)
+	}
+
+	console.log('Server started on port 4444')
+})
